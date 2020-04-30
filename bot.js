0c41c18bbba8b10029d337c4bf4857fcc536df63
Brandon Gilbertson <brandong9@sky.com>
2020-04-30 13:48:45 +0100

    Event and Command Handler

---
+++ bot.js
@@ -0,0 +1,16 @@
+// -- Requiring modules
+const { Client, Collection } = require('discord.js');
+const { config } = require('dotenv');
+const bot = new Client();
+
+// -- Setting .env path
+config({
+  path: __dirname + '/.env',
+});
+
+['aliases', 'commands'].forEach((x) => (bot[x] = new Collection()));
+['command', 'event'].forEach((x) => require(`./handlers/${x}`)(bot));
+
+// ---Logging in with token or test token---
+const token = process.env.TEST == 'true' ? process.env.TESTTOKEN : process.env.TOKEN;
+bot.login(token);
---
+++ events/guild/message.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const env = process.env;
+const prefix = env.PREFIX;
+
+module.exports = async (bot, message) => { 
+  if(message.author.bot || message.channel.type === "dm") return;
+
+  let args = message.content.slice(prefix.length).trim().split(/ +/g);
+  let cmd = args.shift().toLowerCase();
+
+  if(!message.content.startsWith(prefix)) return;
+  let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
+  if(commandfile) commandfile.run(bot, message, args)
+}
