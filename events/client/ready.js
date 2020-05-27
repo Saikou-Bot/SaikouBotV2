1ce2fc178b431db3bf4c78f4aa2cf4b3e0457c8a
Brandon Gilbertson <brandong9@sky.com>
2020-05-27 14:45:38 +0100

    Status correct users fix

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,8 +1,12 @@
 module.exports = (bot) => {
   console.log(`\n${bot.user.username} has loaded successfully and is online. \nServers: ${bot.guilds.cache.size}   Users: ${bot.users.cache.size}   Channels: ${bot.channels.cache.size}`);
 
-  const statuses = [`${bot.users.cache.size} users | .help`, `${bot.users.cache.size} users`, `Saikou Staff | .help`];
+  const guild = bot.guilds.cache.get("397791695514894341");
+  guild.members.fetch()
 
+  const statuses = [`${guild.memberCount} users | .help`, `${guild.memberCount} users`, `Saikou Staff | .help`];
+
+  bot.user.setStatus('idle')
   setInterval(() => {
     let status = statuses[Math.floor(Math.random() * statuses.length)];
     bot.user.setActivity(status, { type: 'LISTENING' });
