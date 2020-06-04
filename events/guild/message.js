8819f718e76b3f163acc6efff4cabda06fecdea0
Harry <krugerh27@gmail.com>
2020-06-04 13:51:58 +0200

    channel config option #4

--- events/guild/message.js
+++ events/guild/message.js
@@ -12,6 +12,14 @@ module.exports = async (bot, message) => {
 
 	if (!message.content.startsWith(prefix)) return;
 	const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
+	if (commandfile.config.channel) {
+		if (message.channel.name.match(commandfile.config.channel) == null) {
+			if (typeof commandfile.error == 'function') {
+				commandfile.error('incorrectChannel', message);
+			}
+			return;
+		};
+	}
 	const alertError = (error) => {
 		console.log(error);
 		message.channel.send('Failed to run command');
