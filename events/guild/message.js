ce4c231f29fccf3d34008fba3f0511790ffef35b
Harry <krugerh27@gmail.com>
2020-06-04 12:58:08 +0200

    Formatted message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -5,12 +5,12 @@ const prefix = env.PREFIX;
 
 
 module.exports = async (bot, message) => {
-  if (message.author.bot || message.channel.type === 'dm') return;
+	if (message.author.bot || message.channel.type === 'dm') return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
 
-  if (!message.content.startsWith(prefix)) return;
-  const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
-  if (commandfile) commandfile.run(bot, message, args);
+	if (!message.content.startsWith(prefix)) return;
+	const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
+	if (commandfile) commandfile.run(bot, message, args);
 };
