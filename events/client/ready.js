89c7c12d0c4136a247f4769c016a0954ceca29c8
Harry <krugerh27@gmail.com>
2020-06-04 13:25:57 +0200

    Made guild configarable and formated ready.js and made it update status faster

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,15 +1,19 @@
 module.exports = (bot) => {
+	const guild = bot.guilds.cache.get(process.env.GUILD);
+	guild.members.fetch();
 
-  const guild = bot.guilds.cache.get('397791695514894341');
-  guild.members.fetch();
+	console.log(`\n${bot.user.username} has loaded successfully and is online. \nServers: ${bot.guilds.cache.size}   Users: ${guild.memberCount}   Channels: ${bot.channels.cache.size}`);
 
-  console.log(`\n${bot.user.username} has loaded successfully and is online. \nServers: ${bot.guilds.cache.size}   Users: ${guild.memberCount}   Channels: ${bot.channels.cache.size}`);
-
-  const statuses = [`${guild.memberCount} users | .help`, `${bot.channels.cache.size} channels`, 'Saikou Staff | .help'];
+	const statuses = [`${guild.memberCount} users | .help`, `${bot.channels.cache.size} channels`, 'Saikou Staff | .help'];
 
 
-  setInterval(() => {
-    const status = statuses[Math.floor(Math.random() * statuses.length)];
-    bot.user.setActivity(status, { type: 'STREAMING', url: 'https://twitch.tv/doingthisforthestatuslol' });
-  }, 15000);
+	function updateStatus() {
+		const status = statuses[Math.floor(Math.random() * statuses.length)];
+		bot.user.setActivity(status, {
+			type: 'STREAMING',
+			url: 'https://twitch.tv/doingthisforthestatuslol'
+		});
+	}
+	updateStatus();
+	setInterval(updateStatus, 15000);
 };
