7e5b548455d8aec591f4ba18b9a206099165ae29
Harry <krugerh27@gmail.com>
2020-06-04 14:56:17 +0200

    Finished methods for cooldown

--- utils/cooldown.js
+++ utils/cooldown.js
@@ -1,9 +1,32 @@
+const extend = require('extend');
+
 const cooldownDefaults = {
 	name: '',
 	cooldown: 1000,
-	nitro: true
+	roles: {
+		"Server Booster": 0.5,
+		"Omega Follower": 0.5
+	}
 }
 
-class Cooldown {
-	constructor(options = {})
+module.exports = class Cooldown {
+	constructor(options = {}) {
+		this.options = extend({}, cooldownDefaults, options);
+		this.users = new Map();
+	}
+	add(userid) {
+		this.users.set(userid, Date.now());
+	}
+	has(userid) {
+		const timestamp = this.users.get(userid);
+		if (timestamp == undefined) return false;
+		else {
+			if (timestamp + this.options.cooldown < Date.now()) {
+				this.users.delete(userid);
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
 }
