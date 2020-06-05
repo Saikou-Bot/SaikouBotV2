860f9034afa752d75dafe6cc1ee0d45f44cdd866
Brandon Gilbertson <brandong9@sky.com>
2020-06-05 14:56:22 +0100

    Warn command permission checks

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
-const { getUserMod } = require('../utils/getUserMod');
 const moment = require('moment');
 
+const { getUserMod } = require('../utils/getUserMod');
 const warnData = require('../../models/warnData');
 const errors = require('.././utils/errors');
 const colours = require('../../jsonFiles/colours.json');
@@ -11,16 +11,23 @@ module.exports = {
         name: 'warn',
         description: 'Reserved for the staff team to warn a user',
         usage: '.warn/givewarn <user>',
-        accessableby: 'Public',
+        accessableby: 'Staff',
         aliases: ['givewarn'],
     },
     run: async (bot, message, args) => {
 
-
         const member = getUserMod(message, args[0]);
+
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+            return errors.noPerms(message, '<Manage Messages>' || message, '.warn');
+        }
 
         if (!member) {
             return errors.noUser(message, 'warn');
+        }
+
+        if (member.hasPermission('MANAGE_MESSAGES')) {
+            return errors.equalPerms(message, 'Manage Messages');
         }
 
         if (!args.slice(1).join(' ')) {
