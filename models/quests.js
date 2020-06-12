8405fe254d93a355689fe7a7d89b45b4b0f336df
Brandon Gilbertson <brandong9@sky.com>
2020-06-12 14:55:34 +0100

    Add quests command

---
+++ models/quests.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const questsSchema = mongoose.Schema({
+    QuestTitle: String,
+    Difficulty: String,
+    Description: String,
+    Reward: Number,
+});
+
+module.exports = mongoose.model('Quests', questsSchema);
