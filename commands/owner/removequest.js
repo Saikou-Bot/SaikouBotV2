75324e95843dc6bdb53a5ad1724cdbaf55f94299
Brandon Gilbertson <brandong9@sky.com>
2020-06-12 14:55:42 +0100

    Remove quests command

---
+++ commands/owner/removequest.js
@@ -0,0 +1,36 @@
+const quests = require('../../models/quests');
+
+module.exports = {
+    config: {
+        name: 'removequest',
+        description: 'Adds a quest to the list!',
+        usage: '.addquest <QuestTitle> <difficulty> <reward> <description>',
+        accessableby: 'Bot Developer',
+        aliases: [],
+    },
+    run: async (bot, message, args) => {
+
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send('This command is limited to bot developers only.');
+        }
+
+        const questName = args.join(' ');
+
+        if (!questName) {
+            return message.channel.send('Input a quest to remove');
+        }
+
+        quests.findOne({ QuestTitle: questName }, (err, quest) => {
+
+            if (!quest) {
+                return message.channel.send('That quest does not exist!');
+            }
+
+            quests.deleteOne({ QuestTitle: questName }).then(
+                message.channel.send('Quest Deleted!'),
+            );
+
+        });
+
+    },
+};
