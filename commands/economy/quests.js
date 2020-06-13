478ea2f873dc135d44d7f6f534397bc45694fb03
Brandon Gilbertson <brandong9@sky.com>
2020-06-13 15:54:12 +0100

    active quests command

---
+++ commands/economy/quests.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require('discord.js');
+const userQuests = require('../../models/userQuests');
+
+
+module.exports = {
+    config: {
+        name: 'quests',
+        description: 'See which quests you are currently doing!',
+        usage: '.quest',
+        accessableby: 'Followers+',
+        aliases: ['viewquests', 'activequests'],
+    },
+    run: async (bot, message) => {
+
+
+        const currentQuests = userQuests.find({ UserID: message.author.id, completed: false });
+
+        const embed = new MessageEmbed()
+            .setTitle('🛡️ Current Quests')
+            .setDescription('All your currently active quests are listed here.')
+            .setColor(message.member.displayHexColor)
+
+
+            ; (await currentQuests).forEach(r => {
+                embed.addField(`${r.Quest} ─ ${r.Difficulty}`, `${r.Description}\n\`REWARD\` **${r.Reward.toLocaleString()}** credits`);
+            });
+
+        message.channel.send(embed);
+
+
+    },
+};
