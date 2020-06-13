adf9a8f056ed83d1a2d244103a863a8f1385160f
Harry <krugerh27@gmail.com>
2020-06-13 16:46:20 +0200

    Warning users

--- events/autoMod/message.js
+++ events/autoMod/message.js
@@ -1,3 +1,6 @@
+const warnData = require('../../models/warnData.js');
+const moment = require('moment');
+
 const AntiSpam = require('discord-anti-spam');
 const antiSpam = new AntiSpam({
     warnThreshold: 3,
@@ -20,3 +23,37 @@ const antiSpam = new AntiSpam({
 module.exports = (client, message) => {
     antiSpam.message(message);
 }
+
+antiSpam.on('warnAdd', member => {
+    warnData.findOne({
+        userID: member.id,
+        guild: member.guild.id
+    }, (err, warnings) => {
+        if (err) return console.error(err);
+        if (!warnings) {
+            warnings = new warnData({
+                userId: member.id,
+                guild: member.guild.id,
+                warns: [{
+                    Moderator: member.client.user.id,
+                    Time: moment().format('MMMM Do YYYY'),
+                    Reason: 'Anti spam'
+                }]
+            });
+            warnings.save();
+        } else {
+            warnData.updateOne({
+                userID: member.id,
+                guild: member.guild.id
+            }, {
+                $push: {
+                    warns: {
+                        'Moderator': member.client.user.id,
+                        'Time': moment().format('MMMM Do YYYY'),
+                        'Reason': 'Anti spam',
+                    },
+                },
+            });
+        }
+    });
+});
