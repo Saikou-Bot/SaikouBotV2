e1922d28fc617c4ab5141703d51c5ea504b921c4
Harry <krugerh27@gmail.com>
2020-06-13 17:56:05 +0200

    switching auto mod to util

--- events/autoMod/message.js
+++ events/autoMod/message.js
@@ -1,4 +1,4 @@
-const warnData = require('../../models/warnData.js');
+const warnUtil = require('../../commands/utils/warn');
 const moment = require('moment');
 
 const AntiSpam = require('discord-anti-spam');
@@ -15,8 +15,6 @@ const antiSpam = new AntiSpam({
     maxDuplicatesBan: 12,
     exemptPermissions: [],
     ignoreBots: true,
-    verbose: true,
-    debug: true,
     ignoredUsers: []
 });
 
@@ -25,35 +23,12 @@ module.exports = (client, message) => {
 }
 
 antiSpam.on('warnAdd', member => {
-    warnData.findOne({
-        userID: member.id,
-        guild: member.guild.id
-    }, (err, warnings) => {
-        if (err) return console.error(err);
-        if (!warnings) {
-            warnings = new warnData({
-                userId: member.id,
-                guild: member.guild.id,
-                warns: [{
-                    Moderator: member.client.user.id,
-                    Time: moment().format('MMMM Do YYYY'),
-                    Reason: 'Anti spam'
-                }]
-            });
-            warnings.save();
-        } else {
-            warnData.updateOne({
-                userID: member.id,
-                guild: member.guild.id
-            }, {
-                $push: {
-                    warns: {
-                        'Moderator': member.client.user.id,
-                        'Time': moment().format('MMMM Do YYYY'),
-                        'Reason': 'Anti spam',
-                    },
-                },
-            });
+    warnUtil.addWarn({
+        user: member.id,
+        guild: member.guild.id,
+        warn: {
+            moderator: member.client.user.id,
+            reason: 'Auto mod'
         }
     });
 });
