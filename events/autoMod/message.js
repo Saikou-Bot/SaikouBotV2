0f7c82e54d6e87528364a6e41fe6a85b8418e6df
Harry <krugerh27@gmail.com>
2020-06-14 19:47:49 +0200

    eslint fixed file

--- events/autoMod/message.js
+++ events/autoMod/message.js
@@ -1,34 +1,33 @@
 const warnUtil = require('../../commands/utils/warn');
-const moment = require('moment');
 
 const AntiSpam = require('discord-anti-spam');
 const antiSpam = new AntiSpam({
-    warnThreshold: 3,
-    kickThreshold: 7,
-    banThreshold: 7,
-    maxInterval: 2000,
-    warnMessage: '{@user}, You have been warned for spamming',
-    kickMessage: '**{user_tag}** has been kicked for spamming.',
-    banMessage: '**{user_tag}** has been banned for spamming.',
-    maxDuplicatesWarning: 7,
-    maxDuplicatesKick: 10,
-    maxDuplicatesBan: 12,
-    exemptPermissions: [],
-    ignoreBots: true,
-    ignoredUsers: []
+	warnThreshold: 3,
+	kickThreshold: 7,
+	banThreshold: 7,
+	maxInterval: 2000,
+	warnMessage: '{@user}, You have been warned for spamming',
+	kickMessage: '**{user_tag}** has been kicked for spamming.',
+	banMessage: '**{user_tag}** has been banned for spamming.',
+	maxDuplicatesWarning: 7,
+	maxDuplicatesKick: 10,
+	maxDuplicatesBan: 12,
+	exemptPermissions: [],
+	ignoreBots: true,
+	ignoredUsers: [],
 });
 
 module.exports = (client, message) => {
-    antiSpam.message(message);
-}
+	antiSpam.message(message);
+};
 
 antiSpam.on('warnAdd', member => {
-    warnUtil.addWarn({
-        user: member.id,
-        guild: member.guild.id,
-        warn: {
-            moderator: member.client.user.id,
-            reason: 'Auto mod'
-        }
-    });
+	warnUtil.addWarn({
+		user: member.id,
+		guild: member.guild.id,
+		warn: {
+			moderator: member.client.user.id,
+			reason: 'Auto mod',
+		},
+	});
 });
