2bcf69c8c50a05c537f99c654f59c02ff5295196
Brandon Gilbertson <brandong9@sky.com>
2020-06-15 14:44:55 +0100

    items + shop (help needed)

---
+++ items/Defences/testitem.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+
+
+const info = {
+    name: 'test',
+    emoji: '⚔️',
+    price: 500,
+};
---
+++ models/items.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const itemsSchema = mongoose.Schema({
+    name: String,
+    inshop: Boolean,
+});
+
+module.exports = mongoose.model('items', itemsSchema);
