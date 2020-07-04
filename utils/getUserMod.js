96d7a01312e65778a74b6a4aa8e5c732eb3fd66d
Harry <krugerh27@gmail.com>
2020-07-04 19:19:59 +0200

    Adding extra utils

---
+++ utils/getUserMod.js
@@ -0,0 +1,23 @@
+function getUserMod(message, toFind = '') {
+	toFind = toFind.toLowerCase();
+	let target = message.guild.members.cache.get(toFind);
+
+	if (!target && message.mentions.members) {
+		target = message.mentions.members.first();
+	}
+
+	if (!target && toFind) {
+		target = message.guild.members.cache.find(member => {
+			return member.displayName.toLowerCase().includes(toFind) || member.user.tag.toLowerCase().includes(toFind);
+		});
+	}
+
+	return target;
+}
+
+module.exports = {
+	name: 'getUserMod',
+	construct(client) {
+		return getUserMod;
+	}
+}
