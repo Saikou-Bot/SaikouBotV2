b1b470db2d8fb9ad96bab5e122464721082661da
Harry <krugerh27@gmail.com>
2020-07-04 19:52:06 +0200

    Moving anti spam to util

--- events/autoMod/message.js
+++ events/autoMod/message.js
@@ -1,35 +1,8 @@
-const AntiSpam = require('discord-anti-spam');
-const antiSpam = new AntiSpam({
-	warnThreshold: 3,
-	kickThreshold: 5,
-	banThreshold: 7,
-	maxInterval: 2000,
-	warnMessage: '{@user}, You have been warned for spamming',
-	kickMessage: '**{user_tag}** has been kicked for spamming.',
-	banMessage: '**{user_tag}** has been banned for spamming.',
-	maxDuplicatesWarning: 3,
-	maxDuplicatesKick: 5,
-	maxDuplicatesBan: 10,
-	maxDuplicatesInterval: 5000,
-	exemptPermissions: [],
-	ignoreBots: true,
-	ignoredUsers: [],
-});
 
 const inviteLink = /discord\.gg|discordapp.com\/invite|discord.com\/invite/m;
 
 module.exports = (client, message) => {
-	const warnUtil = client.utils.warn;
-	antiSpam.on('warnAdd', member => {
-		warnUtil.addWarn({
-			user: member.id,
-			guild: member.guild.id,
-			warn: {
-				moderator: member.client.user.id,
-				reason: 'Auto mod',
-			},
-		});
-	});
+	const { antiSpam, warn: warnUtil } = client.utils;
 	antiSpam.message(message);
 	if (inviteLink.test(message.content)) {
 		message.delete();
