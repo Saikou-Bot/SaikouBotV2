e41cce03f8596180b93aaa78799afec6e53017ea
Brandon Gilbertson <brandong9@sky.com>
2020-08-20 15:32:20 +0100

    Skillrate mission economy removal

--- commands/fun/skillrate.js
+++ commands/fun/skillrate.js
@@ -1,6 +1,3 @@
-const userData = require('../../models/userData.js');
-const userQuests = require('../../models/userQuests');
-
 module.exports = {
 	config: {
 		name: 'skillrate',
@@ -19,7 +16,7 @@ module.exports = {
 		if (chance < 91) {
 			const skillrateEmbed = new MessageEmbed()
 				.setTitle('Skill Rating')
-				.setDescription(`**${member.displayName}** is \`%${rating}\` skilled! 🏆`)
+				.setDescription(`**${member.displayName}** is \`${rating}%\` skilled! 🏆`)
 				.setColor(member.displayHexColor);
 
 			message.channel.send(skillrateEmbed);
@@ -34,24 +31,6 @@ module.exports = {
 			message.channel.send(skillrateEmbed);
 		}
 
-
-		userData.findOne({ userID: message.author.id }, (err, UserData) => {
-
-			userQuests.findOne({ UserID: message.author.id, Quest: 'Skilled Fighter', completed: false }, (err, SkilledFighter) => {
-
-				if (rating === 100 && SkilledFighter) {
-					message.channel.send(`You completed the quest **Skilled Fighter** and was rewarded ${SkilledFighter.Reward.toLocaleString()} credits.`);
-
-					UserData.coins += SkilledFighter.Reward;
-
-					SkilledFighter.completed = true;
-					SkilledFighter.save();
-
-
-				}
-				UserData.save();
-			});
-		});
 
 	},
 };
