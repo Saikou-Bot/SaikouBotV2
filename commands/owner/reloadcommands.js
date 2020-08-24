cdbe3c4bc239ac8b93254c21bb15eec4a9904805
Harry <krugerh27@gmail.com>
2020-08-24 12:24:56 +0200

    Adding reload all command

---
+++ commands/owner/reloadcommands.js
@@ -0,0 +1,19 @@
+module.exports = {
+	config: {
+		name: 'reloadcommands'
+	},
+	async run({ message, utils }) {
+		const { author } = message;
+
+		if (!process.env.OWNERS.includes(author.id)) return message.reply('Owner only');
+		try {
+			utils.commands.loadAll();
+		}
+		catch(err) {
+			console.error(err);
+			return message.reply('Failed loading commands');
+		}
+
+		message.reply('Succesfully loaded commands');
+	}
+}
