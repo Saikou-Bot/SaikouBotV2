b57a121bbb98c94906db95e6dedddadad27066c8
Harry <krugerh27@gmail.com>
2020-08-24 12:01:36 +0200

    Adding reload command and function and listening to all events

--- handlers/command.js
+++ handlers/command.js
@@ -1,21 +1,17 @@
 const { readdirSync } = require('fs');
+const chalk = require('chalk');
 
 module.exports = (bot) => {
 	const load = dirs => {
 		const commands = readdirSync(`./commands/${dirs}/`).filter(d => d.endsWith('.js'));
 		for (const file of commands) {
-			const pull = require(`../commands/${dirs}/${file}`);
-			if (!pull || !pull.config) continue;
-			if (pull.config.cooldown) {
-				pull.cooldown = new bot.utils.cooldown({
-					name: pull.config.name,
-					cooldown: pull.config.cooldown === true ? undefined : pull.config.cooldown,
-					roles: pull.config.cooldownRoles,
-				});
+			try {
+				bot.utils.loadCommand(`${dirs}/${file}`);
 			}
-			pull.path = `${dirs}/${file}`;
-			bot.commands.set(pull.config.name, pull);
-			if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
+			catch(err) {
+				console.error(err);
+				console.error(`${chalk.bgYellow('Failed')} loading command ${chalk.bold(file)}`);
+			}
 		}
 	};
 	['information', 'moderation', 'fun', 'economy', 'games', 'general', 'owner'].forEach(x => load(x));
--- utils/watch.js
+++ utils/watch.js
@@ -1,23 +1,45 @@
 const fs = require('fs');
 const Path = require('path');
 const chokidar = require('chokidar');
+const chalk = require('chalk');
 
 class CommandWatcher {
 	constructor(client, commandsPath) {
 		this.client = client;
 		this.commandsPath = commandsPath;
 		this.watcher = chokidar.watch(commandsPath)
-			.on('change', (path) => {
-				const dirname = Path.basename(Path.dirname(path));
-				const filename = Path.basename(path);
-
-				this.client.utils.loadCommand(`${dirname}/${filename}`);
-				console.log(`${filename} restarted`);
+			.on('ready', () => {
+				this.watcher.on('change', (path) => this.reloadCommand(path))
+				.on('add', (path) => this.reloadCommand(path))
+				.on('unlink', (path) => {
+					const { dirname, filename } = this.parse(path);
+					const fullpath = `${dirname}/${filename}`;
+					console.log('fullpath: ', fullpath)
+					let commandFile = this.client.commands.find(c => c.path == fullpath);
+					if (!commandFile) return;
+					if (commandFile.aliases) commandFile.aliases.keys().forEach(alias => this.client.aliases.delete(alias));
+					this.client.commands.delete(commandFile.name);
+					console.log(`${chalk.bold(filename)} command ${chalk.bgRed('deleted')}`)
+				});
 			});
 	}
-	reloadCommand(command) {
-		const pull = this.client.commands.get(command);
-		return client.utils.loadCommand(Path.join(this.commandsPath, pull.path));
+	parse(path) {
+		return {
+			dirname: Path.basename(Path.dirname(path)),
+			filename: Path.basename(path)
+		}
+	}
+	reloadCommand(path) {
+		const { dirname, filename } = this.parse(path);
+		try {
+			this.client.utils.loadCommand(`${dirname}/${filename}`);
+		}
+		catch(err) {
+			console.error(err);
+			console.error(`${chalk.bgYellow('Failed')} loading command ${chalk.bold(filename)}`);
+			return;
+		}
+		console.log(`${chalk.bold(filename)} loaded`);
 	}
 }
 
