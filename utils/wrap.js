7f74870559beed4e23eab4606f05d307ee507663
Harry <krugerh27@gmail.com>
2020-09-20 16:05:13 +0200

    eslint fix

--- utils/wrap.js
+++ utils/wrap.js
@@ -1,8 +1,8 @@
 module.exports = {
 	name: 'wrap',
 	construct(client) {
-		return function wrap(msg, charLimit = 2000, wrapChar = "\n") {
-			let messages = [];
+		return function wrap(msg, charLimit = 2000, wrapChar = '\n') {
+			const messages = [];
 			while (msg.length > charLimit) {
 				const firstPart = msg.slice(0, charLimit);
 				const lastNewline = firstPart.lastIndexOf('\n');
@@ -11,6 +11,6 @@ module.exports = {
 			}
 			if (msg) messages.push(msg);
 			return messages;
-		}
+		};
 	}
-}
+};
