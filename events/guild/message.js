907ee3a7a90084bfa8d5edd0a860fb8e949e5c10
Harry <krugerh27@gmail.com>
2020-09-22 17:40:39 +0200

    Moving channel limitations to guild/message

--- events/guild/message.js
+++ events/guild/message.js
@@ -31,6 +31,10 @@ function error(name, command = {}, callback, ...args) {
 
 module.exports = async (bot, message) => {
 	if (message.author.bot || message.channel.type === 'dm') return;
+
+	if (message.channel.name.match('suggestions')) message.delete().catch(() => {});
+	if (message.channel.name.match('art') && ( message.attachments.size < 1 || !message.content.startsWith('https') )) return message.delete().catch(() => {}); // TODO: Add more advanced image detection
+
 	if (!message.content.startsWith(prefix)) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
