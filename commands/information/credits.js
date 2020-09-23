2c0d799b2922a60cd0ba3de1955ea73a876c6a6e
Brandon Gilbertson <brandong9@sky.com>
2020-09-23 13:06:47 +0100

    Adding testers to credits command

--- commands/information/credits.js
+++ commands/information/credits.js
@@ -15,7 +15,7 @@ module.exports = {
 			.setTitle('📄 Credits')
 			.setDescription('Big thanks to the following people who have helped Saikou bot become a reality, without them the bot wouldn\'t be where it is today.')
 			.addField('→ Bot Developers:', '<@229142187382669312> `[Head Developer]`\n<@670588428970098708> `[Developer]`')
-			.addField('→ Contributors:', '<@341317140655243266> `[Helper/Tester]`')
+			.addField('→ Contributors:', '<@341317140655243266> `[Helper/Tester]`\n<@216266838156378114> `[Tester]`\n<@670080884249985085> `[Tester]`\n<@202547908015423488> `[Tester]`\n<@697864119302225952> `[Tester]`\n<@458023820129992716> `[Tester]`')
 			.setColor(colours.blurple);
 
 		message.channel.send(credits);
