c76b21a9d351c9b6ff2e388cb4f01a90bdd9dff2
Harry <krugerh27@gmail.com>
2020-09-25 09:09:04 +0200

    Removing extra autoMod stuff

--- events/autoMod/message.js
+++ events/autoMod/message.js
@@ -10,19 +10,21 @@ module.exports = async (client, message) => {
 		}
 	}
 	if (message.author.bot) return;
-	const { antiSpam, warn: warnUtil, swearFilter } = client.utils;
-	antiSpam.message(message);
-	if (inviteLink.test(message.content)) {
-		message.delete().catch(() => {});
-		message.reply('You have been warned for sending invite');
-		warnUtil.addWarn({
-			user: message.author.id,
-			guild: message.guild.id,
-			warn: {
-				moderator: message.client.user.id,
-				reason: 'Invite link'
-			}
-		});
-	}
-	else {swearFilter(message);}
+	// const { antiSpam, warn: warnUtil, swearFilter } = client.utils;
+	const { swearFilter } = client.utils;
+	swearFilter(message);
+	// antiSpam.message(message);
+	// if (inviteLink.test(message.content)) {
+	// 	message.delete().catch(() => {});
+	// 	message.reply('You have been warned for sending invite');
+	// 	warnUtil.addWarn({
+	// 		user: message.author.id,
+	// 		guild: message.guild.id,
+	// 		warn: {
+	// 			moderator: message.client.user.id,
+	// 			reason: 'Invite link'
+	// 		}
+	// 	});
+	// }
+	// else {swearFilter(message);}
 };
