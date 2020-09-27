45fe99cd9d3329d961c8dc6dd92bfa7694b91bae
Harry <krugerh27@gmail.com>
2020-09-27 13:28:33 +0200

    Adding database importer script

---
+++ scripts/databaseImporter.js
@@ -0,0 +1,44 @@
+global.mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Warn = require('../models/warnData');
+
+const guildID = '704621852231729212';
+
+dotenv.config({
+	path: __dirname + '/../.env'
+});
+
+const logs = require('./warnlogs.json');
+
+mongoose.connect(process.env.MONGOPASSWORD, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true
+})
+	.then(async () => {
+		for (var i = 0; i < logs.length; i++) {
+			const log = logs[i];
+			if (log.type != 'Warn') continue;
+
+			const warn = {
+				Moderator: log.mod.id,
+				Time: new Date(log.createdAt),
+				Reason: log.reason
+			};
+
+			let userWarn = await Warn.findOne({ userID: log.user.id });
+			if (!userWarn) {
+				userWarn = new Warn({
+					userID: log.user.id,
+					warns: [],
+					guild: guildID
+				});
+			}
+
+			userWarn.warns.push(warn);
+			await userWarn.save();
+			console.log('Added warn for', log.user.username);
+		}
+		mongoose.connection.close();
+	});
