da703913dfe66a9f138605daa2017b2af3b7d31b
Harry <krugerh27@gmail.com>
2020-10-01 15:24:15 +0200

    Fixing shorten util

--- utils/shorten.js
+++ utils/shorten.js
@@ -1,10 +1,11 @@
 function shorten(str, length = 2048, endStr = '...') {
-	return str.length < length ? str.substring(0, length - endStr.length) + endStr : str;
+	return str.length > length ? str.substring(0, length - endStr.length) + endStr : str;
 }
 
 module.exports = {
 	name: 'shorten',
 	construct(client) {
 		return shorten;
-	}
+	},
+	shorten
 };
