5cad64c26bc63ad7c53de663a228b21fafea8c69
Harry <krugerh27@gmail.com>
2020-10-21 21:45:04 +0200

    eslint fix

--- commands/moderation/delete-warn.js
+++ commands/moderation/delete-warn.js
@@ -11,25 +11,27 @@ module.exports = {
 	async run({ message, args, utils: { getUserMod }, databases }) {
 		const warn = await Warn.findById(args.warnID);
 
-		if (!warn) return message.channel.send(new Discord.MessageEmbed({
-					title: 'No such warn',
-					color: colours.red
-				}));
+		if (!warn) {
+			return message.channel.send(new discord.MessageEmbed({
+				title: 'No such warn',
+				color: colours.red
+			}));
+		}
 
 		try {
 			await warn.remove();
 		}
 		catch(err) {
 			console.error(err);
-			return message.channel.send(new Discord.MessageEmbed({
+			return message.channel.send(new discord.MessageEmbed({
 				title: 'Failed to delete warn',
 				color: colours.red
 			}));
 		}
 
-		return message.channel.send(new Discord.MessageEmbed({
+		return message.channel.send(new discord.MessageEmbed({
 			title: 'Succesfully deleted warn',
 			color: colours.green
 		}));
 	}
-}
+};
--- models/warn.js
+++ models/warn.js
@@ -1,7 +1,7 @@
 if (!mongoose) var mongoose = require('mongoose');
 
 const warnSchema = new mongoose.Schema({
-	Moderator: { type: String, required: true},
+	Moderator: { type: String, required: true },
 	Time: { type: Date, required: true },
 	Reason: { type: String, required: true }
 });
