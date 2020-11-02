54b91a5ee7672fe180b311af02adc11f93449c61
Harry <krugerh27@gmail.com>
2020-11-02 13:39:21 +0200

    Refactoring 8ball

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,3 +1,26 @@
+const replies = [
+	'It is certain',
+	'It is decidedly so',
+	'Without a doubt',
+	'Yes - definitely',
+	'You may rely on it',
+	'As I see it, yes',
+	'Most likely',
+	'Outlook good',
+	'Yes',
+	'Signs point to yes',
+	'Reply hazy, try again',
+	'Ask again later',
+	'Better to not tell you now',
+	'Cannot predict now',
+	'Don\'t count on it',
+	'My reply is no',
+	'My sources say no',
+	'Outlook not so good',
+	'Very doubtful',
+	'Certainly not',
+];
+
 module.exports = {
 	config: {
 		name: '8ball',
@@ -9,53 +32,26 @@ module.exports = {
 		cooldown: true,
 		autoCooldown: true,
 	},
-	run: async ({ client: bot, message, args }) => {
+	run: async ({ message, args, argString }) => {
 
-		const replies = [
-			'It is certain',
-			'It is decidedly so',
-			'Without a doubt',
-			'Yes - definitely',
-			'You may rely on it',
-			'As I see it, yes',
-			'Most likely',
-			'Outlook good',
-			'Yes',
-			'Signs point to yes',
-			'Reply hazy, try again',
-			'Ask again later',
-			'Better to not tell you now',
-			'Cannot predict now',
-			'Don\'t count on it',
-			'My reply is no',
-			'My sources say no',
-			'Outlook not so good',
-			'Very doubtful',
-			'Certainly not',
-		];
 
-		const result = Math.floor((Math.random() * replies.length));
-		const question = args.join(' ');
-		const questionShortener = question.length > 1900 ? question.substring(0, 1800) + '...' : question;
-
-		const Usage = new MessageEmbed()
+		const question = argString;
+		if (!question) return message.channel.send(new MessageEmbed()
 			.setTitle('✍️ No question asked!')
 			.setDescription('Please ask a question for the 8ball to answer.')
 			.setFooter('Input question')
 			.setTimestamp()
-			.setColor(colours.red);
+			.setColor(colours.red));
 
-		if (!args[1]) return message.channel.send(Usage);
+		const questionShortener = question.length > 1900 ? question.substring(0, 1800) + '...' : question;
+		const result = Math.floor((Math.random() * replies.length));
 
-		const ballembed = new MessageEmbed()
+		message.channel.send(new MessageEmbed()
 			.setTitle(':8ball: 8ball Results')
 			.setDescription(`Question\n**${questionShortener}**\n\n8ball Answer\n**${replies[result]}**`)
 			.setThumbnail(message.author.displayAvatarURL())
 			.setFooter(`Asked by ${message.author.tag}`, message.author.displayAvatarURL())
 			.setTimestamp()
-			.setColor(colours.blurple);
-
-
-		message.channel.send(ballembed);
-	},
+			.setColor(colours.blurple));
+	}
 };
