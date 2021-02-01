7717e43978a9f16ff62c8ea6a95ab17e032f8fa0
Harry <krugerh27@gmail.com>
2021-02-01 14:16:55 +0200

    Changing embed color

--- src/structure/SaikouEmbed.js
+++ src/structure/SaikouEmbed.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 class SaikouEmbed extends MessageEmbed {
 	setup(data, skipValidation) {
-		super.setup(Object.assign({}, { color: 1710618 }, data), skipValidation);
+		super.setup(Object.assign({}, { color: 7506394 }, data), skipValidation);
 	}
 }
 module.exports = SaikouEmbed;
