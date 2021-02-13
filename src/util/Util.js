1230dcaf0b4fdd8d0b742bbb17abe105669c95a1
Harry <krugerh27@gmail.com>
2021-02-13 11:05:18 +0200

    Adding choose to util

--- src/util/Util.js
+++ src/util/Util.js
@@ -3,10 +3,16 @@ class Util {
 		throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
 	}
 	// source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-	static getRandomInt(min = 0, max = 1) {
+	static getRandomInt(min, max) {
+		if (min && !max) { max = min; min = 0}
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+	}
+	static choose(arr) {
+		if (!Array.isArray(arr) || arr.length < 1) return null;
+
+		return arr[this.getRandomInt(arr.lenght)];
 	}
 }
 module.exports = Util;
