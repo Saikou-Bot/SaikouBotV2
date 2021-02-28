50c7493666a1801b51ab8778836ae4da0f8cd8fd
Harry <krugerh27@gmail.com>
2021-02-28 15:21:39 +0200

    feat(util) adding chooseValue and chooseKey

--- src/util/Util.js
+++ src/util/Util.js
@@ -17,6 +17,12 @@ class Util {
 
 		return arr[Util.getRandomInt(arr.length)];
 	}
+	static chooseValue(obj) {
+		return this.choose(Object.values(obj))
+	}
+	static chooseKey(obj) {
+		return this.choose(Object.keys(obj));
+	}
 	static mutualGuilds(guilds, user) {
 		if (!user) return null;
 
