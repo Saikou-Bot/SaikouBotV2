4d1ad2d6c199bf11798856a169fc0fa349d9fddf
Harry <krugerh27@gmail.com>
2021-03-17 18:44:24 +0200

    refactor(eslint)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const SaikouBot = require('./client/SaikouBot');
-const chalk = require('chalk');
+// const chalk = require('chalk');
 
 let config = {};
 try {
-    config = require('../config.json');
+	config = require('../config.json');
 }
 catch(err) {
-    console.error(err);
+	console.error(err);
 }
 
 const bot = new SaikouBot(config);
