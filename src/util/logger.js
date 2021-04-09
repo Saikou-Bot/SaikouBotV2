6f7349ff28bc840a306bbb368e6573d1c3f137af
Harry <krugerh27@gmail.com>
2021-04-09 14:39:08 +0200

    feat(logger) adding debug sub debug

--- src/util/logger.js
+++ src/util/logger.js
@@ -5,5 +5,6 @@ logger.log = console.log.bind(console);
 
 logger.info = logger.extend('info');
 logger.warn = logger.extend('warn');
+logger.debug = logger.extend('debug');
 
 module.exports = logger;
