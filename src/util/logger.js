c9e9689c312845df3b05471ea249d66b9f422938
Harry <krugerh27@gmail.com>
2021-04-09 14:38:49 +0200

    fix(logger) fixing vscode debug issue

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,6 +1,7 @@
 const debug = require('debug');
 
 const logger = debug('bot');
+logger.log = console.log.bind(console);
 
 logger.info = logger.extend('info');
 logger.warn = logger.extend('warn');
